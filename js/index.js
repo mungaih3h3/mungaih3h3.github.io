32bbd8004e5795c5baa845fa12d96e3c8c455656
mungai <mungaihaha@gmail.com>
2019-01-05 17:26:04 +0300

    removed scroll reveal for mobiles because it does not work right

--- js/index.js
+++ js/index.js
@@ -1,36 +1,47 @@
-ScrollReveal().reveal("#about", { delay: 100, distance: "50px" });
+ScrollReveal().reveal("#about", {
+  delay: 100,
+  distance: "50px",
+  mobile: false
+});
 ScrollReveal().reveal("#left-why", {
   origin: "bottom",
   distance: "100px",
-  delay: 300
+  delay: 300,
+  mobile: false
 });
 ScrollReveal().reveal("#right-why", {
   origin: "bottom",
   distance: "100px",
-  delay: 300
+  delay: 300,
+  mobile: false
 });
 ScrollReveal().reveal("#why", {
   origin: "bottom",
   distance: "100px",
-  delay: 250
+  delay: 250,
+  mobile: false
 });
 ScrollReveal().reveal("#services", {
   delay: 500,
   origin: "bottom",
-  distance: "150px"
+  distance: "150px",
+  mobile: false
 });
 ScrollReveal().reveal("#left-showcase", {
   delay: 500,
   origin: "left",
-  distance: "500px"
+  distance: "500px",
+  mobile: false
 });
 ScrollReveal().reveal("#right-showcase", {
   delay: 500,
   origin: "right",
-  distance: "500px"
+  distance: "500px",
+  mobile: false
 });
 ScrollReveal().reveal("#contact-stuff", {
   delay: 600,
   origin: "bottom",
-  distance: "250px"
+  distance: "250px",
+  mobile: false
 });
