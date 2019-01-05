c34574bc7150856b01f06e277d911cec7ec3c8f5
mungai <mungaihaha@gmail.com>
2019-01-05 17:06:33 +0300

    added some animations

---
+++ js/index.js
@@ -0,0 +1,36 @@
+ScrollReveal().reveal("#about", { delay: 100, distance: "50px" });
+ScrollReveal().reveal("#left-why", {
+  origin: "bottom",
+  distance: "100px",
+  delay: 300
+});
+ScrollReveal().reveal("#right-why", {
+  origin: "bottom",
+  distance: "100px",
+  delay: 300
+});
+ScrollReveal().reveal("#why", {
+  origin: "bottom",
+  distance: "100px",
+  delay: 250
+});
+ScrollReveal().reveal("#services", {
+  delay: 500,
+  origin: "bottom",
+  distance: "150px"
+});
+ScrollReveal().reveal("#left-showcase", {
+  delay: 500,
+  origin: "left",
+  distance: "500px"
+});
+ScrollReveal().reveal("#right-showcase", {
+  delay: 500,
+  origin: "right",
+  distance: "500px"
+});
+ScrollReveal().reveal("#contact-stuff", {
+  delay: 600,
+  origin: "bottom",
+  distance: "250px"
+});
